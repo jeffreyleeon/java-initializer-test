338eab85dbfb92c9107530a7b866f5da52de54fc
Lee Chun On <jeffreyleeon@gmail.com>
2017-06-09 10:31:57 +0800

    Add a public constructor

--- Test.java
+++ Test.java
@@ -7,6 +7,10 @@ class Test {
         System.out.println("This is a non-static initializer");
     }
 
+    public Test() {
+        System.out.println("This is a public constructor");
+    }
+
     public static void main(String[] args) {
         System.out.println("This is the main function");
         Test t = new Test();
